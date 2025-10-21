595fff83511c27df487ad65706283cb3fe1a1522
Andy Zhou <admin@lakeblvd.net>
2025-10-21 15:19:20 +1100

    updated change password form; create delete profile form.

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
-from wtforms import PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError, AnyOf
+from wtforms import BooleanField, PasswordField, SubmitField
 
 class ChangePasswordForm(FlaskForm):
     current_password = PasswordField("Current password:")
     new_password = PasswordField("New password:")
-    confirm_password = PasswordField("Confirm password:")
+    confirm_password = PasswordField(
+        "Confirm password:",
+        validators=[DataRequired(), EqualTo('new_password', message='password must match')])
     submit = SubmitField("Submit")
 
+class DeleteProfileForm(FlaskForm):
+    confirm_delete = BooleanField(
+        label="I confirm to permanently delete my account.",
+        validators=[AnyOf([True])])
+    submit = SubmitField("Submit")
