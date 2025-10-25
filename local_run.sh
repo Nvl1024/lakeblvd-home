e076c77af09f8db06c96a2ed689871e60d03a5d4
Andy Zhou <admin@lakeblvd.net>
2025-10-25 23:20:21 +1100

    repository utility maintenance

---
+++ local_run.sh
@@ -0,0 +1,6 @@
+# set up environment variables
+export APP_ENV="dev"
+export FLASK_KEY="b72ee9b7-3c6e-470b-a912-007df5bca4e8"
+export DATABASE_URL="sqlite:///lakeblvd-home.db"
+# run flask with gunicorn
+gunicorn -b 127.0.0.1:5000 "app:create_app()" 
