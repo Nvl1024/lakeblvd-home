bd5b6f880a60f7d8de47f663a0a0dcc956701e20
Andy Zhou <admin@lakeblvd.net>
2025-10-27 17:07:55 +1100

    about page: fixed markdown & html template handling

--- app/home/routes.py
+++ app/home/routes.py
@@ -4,6 +4,7 @@ lakeblvd.net homepage
 from flask import render_template, request
 from flask_login import current_user, login_required
 import markdown
+import bleach
 from . import bp
 
 @bp.route('/', methods=["GET"])
@@ -12,8 +13,12 @@ def index():
 
 @bp.route('/about', methods=["GET", "POST"])
 def about():
-    with open('app/static/home/about.md') as file:
+    with open('app/static/home/about.md', 'r', encoding="utf-8") as file:
         md_content = file.read()
-    md_html = markdown.markdown(md_content)
-    return render_template("home/about-md.html", content=md_html)
+    md_html = markdown.markdown(md_content, extensions=["fenced_code", "tables"])
+    # clean up the html
+    allowed_tags = list(bleach.sanitizer.ALLOWED_TAGS) + ["p", "pre", "code", "h1", "h2", "h3", "table", "thead", "tbody", "tr", "th", "td", "ul", "ol", "li", "blockquote"]
+    allowed_attrs = {"a": ["href", "title", "rel", "target"], "img": ["src", "alt", "title"]}
+    safe_html = bleach.clean(md_html, tags=allowed_tags, attributes=allowed_attrs, strip=True)
+    return render_template("home/about-md.html", content=safe_html)
 
--- app/static/home/about.md
+++ app/static/home/about.md
@@ -1,10 +1,10 @@
-### About the website
+## About the website
 This is a central homepage to my small web tools and personal experiments.
 
 ### About me
-I'm a software developer specialized in test automation, machine learning, and web development. You can find my profile on (https://github.com/Nvl1024)[GitHub].
+I'm a software developer specialized in test automation, machine learning, and web development. You can find my profile on [GitHub](https://github.com/Nvl1024).
 
-Already received the invitation code? Register to access features.
+Already received the invitation code? [Register](/register) to access features.
 
 *Planned features:*
 - Note taking with two-factor encrypted cookie storage
