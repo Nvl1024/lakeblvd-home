4cda49993362b5d2b74c9880f5929e84ae1d986f
Andy Zhou <admin@lakeblvd.net>
2025-11-03 15:12:29 +1100

    script to inject temporary invite code, for prod environment

---
+++ test/inject-temp-code.py
@@ -0,0 +1,22 @@
+"""
+inject temporary invite code
+into production environment from envar
+"""
+import os
+from injection import *
+
+def main():
+    """
+    execute inject invite code
+    """
+    tempcode = os.getenv("TEMP_INVITE_CODE")
+    if tempcode is None:
+        raise AttributeError("TEMP_INVITE_CODE is None")
+    assert isinstance(tempcode, str), \
+        f"expecting tempcode to be string, not {type(tempcode)}"
+    inject_code = InjectInviteCode()
+    inject_code.insert(code=tempcode)
+    print('done')
+    
+if __name__ == '__main__':
+    main()
